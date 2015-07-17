87bf4a69e3a867e8a1614505668adda6684bd264
rupertbates <rupert.bates@guardian.co.uk>
2015-07-17 16:36:07 +0100

    Row sorting logic

--- app/src/main/java/com/jawnnypoo/physicslayout/sample/TypicalFragment.java
+++ app/src/main/java/com/jawnnypoo/physicslayout/sample/TypicalFragment.java
@@ -1,6 +1,7 @@
 package com.jawnnypoo.physicslayout.sample;
 
 import android.content.Context;
+import android.graphics.drawable.GradientDrawable;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.Gravity;
@@ -11,30 +12,31 @@ import android.widget.BaseAdapter;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import com.jawnnypoo.physicslayout.Physics;
+import com.jawnnypoo.physicslayout.PhysicsConfig;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * This may seem familiar....
  * Created by Jawn on 5/5/2015.
  */
 public class TypicalFragment extends Fragment {
-    String[] values = new String[]{"Art"
+    List<String> values = new ArrayList<>(Arrays.asList(new String[]{
+            "Art"
             , "Culture"
             , "Performance art"
-            , "Art and design"
             , "Women"
             , "Photography"
             , "Superhero movies"
             , "Culture"
+            , "Science fiction and fantasy"
             , "Film"
             , "Marvel"
             , "DC Comics"
-            , "Science fiction and fantasy"
-            , "Ant-Man"
-            , "X-Men Origins: Wolverine"
-            , "Architecture"
-            , "Art and design"
-            , "London"
-            , "Culture"
-            , "Cities"};
+            , "Ant-Man"}));
 
     public static TypicalFragment newInstance() {
         TypicalFragment fragment = new TypicalFragment();
@@ -54,8 +56,8 @@ public class TypicalFragment extends Fragment {
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        listView = (PhysicsListView) view;
-        listView.setAdapter(new BubbleAdapter(getActivity(), values));
+        listView = (PhysicsListView) view.findViewById(R.id.list);
+        listView.setAdapter(new BubbleAdapter(getActivity(), new BubbleRowSorter(32).getRows(values)));
     }
 
     @Override
@@ -63,18 +65,22 @@ public class TypicalFragment extends Fragment {
         super.onResume();
     }
 
-    static class BubbleAdapter extends BaseAdapter {
+    static class BubbleAdapter extends BaseAdapter implements View.OnClickListener {
+        private static final int MARGIN = 6;
         private final Context context;
-        private String[] values;
+        private final LayoutInflater inflater;
+        private List<String[]> rows;
+        private List<String> selected = new ArrayList<>();
 
-        public BubbleAdapter(Context context, String[] values) {
+        public BubbleAdapter(Context context, List<String[]> rows) {
             this.context = context;
-            this.values = values;
+            this.inflater = LayoutInflater.from(context);
+            this.rows = rows;
         }
 
         @Override
         public int getCount() {
-            return values.length;
+            return rows.size();
         }
 
         @Override
@@ -89,17 +95,53 @@ public class TypicalFragment extends Fragment {
 
         @Override
         public View getView(int position, View convertView, ViewGroup parent) {
-            TextView child = new SquareTextView(context);
-            child.setText(values[position]);
-            child.setBackgroundDrawable(context.getResources().getDrawable(R.drawable.circle));
-            int max = 300;
-            int padding = 20;
-            child.setPadding(padding, padding, padding, padding);
-            child.setMaxWidth(max);
-            child.setMaxHeight(max);
-            child.setGravity(Gravity.CENTER);
-            child.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+            LinearLayout wrapper = getWrapper();
+            for (String row : rows.get(position)) {
+                wrapper.addView(getChild(row, parent));
+            }
+            return wrapper;
+
+        }
+
+        private View getChild(String text, ViewGroup parent) {
+            TextView child = (TextView) inflater.inflate(R.layout.bubble, parent, false);
+            child.setTag(text);
+            child.setText(text);
+            child.setOnClickListener(this);
+            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+            params.setMargins(MARGIN, MARGIN, MARGIN, MARGIN);
+            child.setLayoutParams(params);
             return child;
+        }
+
+        private LinearLayout getWrapper() {
+            LinearLayout layout = new LinearLayout(context);
+            //int padding = 6;
+            //layout.setPadding(padding, padding, padding, padding);
+            layout.setGravity(Gravity.CENTER);
+            Physics.setPhysicsConfig(
+                    layout,
+                    new PhysicsConfig.Builder()
+                            .setDensity(9f)
+                            .setFriction(0f)
+                            .setRestitution(0.5f)
+                            .setAllowRotation(false)
+                            .build());
+            return layout;
+        }
+
+        @Override
+        public void onClick(View v) {
+            GradientDrawable gradientDrawable = (GradientDrawable) v.getBackground().mutate();
+            String text = (String) v.getTag();
+            if (selected.contains(text)) {
+                gradientDrawable.setColor(context.getResources().getColor(R.color.background));
+                selected.remove(text);
+            } else {
+                gradientDrawable.setColor(context.getResources().getColor(R.color.highlight));
+                selected.add(text);
+            }
+
         }
     }
 }
